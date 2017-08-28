eaa370c4c7c93b2058371801f256c8aab826d701
dtripathi05 <dtripathi@tavisca.com>
2017-08-28 11:07:21 +0530

    Modified Via JQuery

--- TicTac.js
+++ TicTac.js
@@ -1,8 +1,5 @@
-function startGame () {
-
-	for(var i=1;i<10;i++){
-		clearBox(i);
-	}
+function startGame () {	
+		clearBox();	
 	document.turn="X";
 	if(Math.random() < 0.5)
 	{
@@ -15,28 +12,27 @@ function setMessage(msg) {
 	document.getElementById("message").innerText = msg;
 }
 function nextMove(box) {
-	if(document.winner != null){
-		setMessage(document.winner+" " +"- Already Won the Game .");		
+	if(document.winner != null){		
+		setMessage("Game is Already Over !");		
 	}
 	else if(box.innerText == ""){		
 		box.innerText=document.turn;
 		switchTurn();			
 	}	 
-	else{
-	if(draw()){		
-			setMessage("Game Draw");		
-	}
-	else		
+	else{	
 		setMessage("That Box is Already Set");
 	}
 }
 function switchTurn(){
 
-	if(checkForWinner(document.turn)){
+	if(checkForWinner(document.turn)=='Won'){		
 		setMessage("Congo"+"- "+ document.turn +" "+"! you have Won the Game ");
 		document.winner = document.turn;		
 	}
-	
+	else if(checkForWinner(document.turn)=='Draw'){
+		setMessage("Game Draw !"+"- ");
+		document.winner = document.turn;		
+	}
 	else if(document.turn =="X")
 	{ 
 		document.turn = "O";
@@ -49,7 +45,7 @@ function switchTurn(){
 	}	
 }
 function checkForWinner(move) {
-	var result=false;
+	var result='None';
 	if(checkRow(1,2,3,move)||
 	   checkRow(4,5,6,move)||
 	   checkRow(7,8,9,move)||
@@ -58,31 +54,36 @@ function checkForWinner(move) {
 	   checkRow(3,6,9,move)||
 	   checkRow(1,5,9,move)||
 	   checkRow(3,5,7,move)){
-		result = true;
+		result = 'Won';
+	}
+	else if (draw()){
+		result = 'Draw';
+
 	}	
 	return result; 
 }
 function checkRow (a,b,c,move) {
 	var result = false;
-	if(getBox(a) == move && getBox(b) == move && getBox(c) == move){
-		result = true;
-	}
-return result;
-}
-function getBox(number) {
-	return document.getElementById("b" + number).innerText;
-}
-
-function clearBox (number) {
-	document.getElementById("b"+ number).innerText="";
-}
-function draw() {
-	var result=false;
-	if(b1!=""&&b2!=""&&b3!=""&&b4!=""&&b5!=""&&b6!=""&&b7!=""&&b8!=""&&b9!="")
-	{
-		result=true; 
+	if($('#b' + a).text()==move && $('#b' + b).text()==move && $('#b' + c).text()==move){
+		result =true;
 	}
 	return result;
+}
+function clearBox () {
+	$('.Box').each(function(){
+		$(this).text("");});
+}
+function draw() {
+	var count ='No';
+	$(".Box").each(function(){
+		if($(this).text()==""){
+			count='Yes';
+		}
+	});
+	 var dict = {};
+        dict.No = true;
+        dict.Yes = false;
+        return dict[count];
 }
 
 	
